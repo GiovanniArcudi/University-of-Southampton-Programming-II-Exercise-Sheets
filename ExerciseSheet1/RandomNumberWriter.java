7af0c4d8c4156d6fdebe25dd4683daa1926634e1
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2020-03-17 15:03:03 +0000

    Update RandomNumberWriter.java

--- ExerciseSheet1/RandomNumberWriter.java
+++ ExerciseSheet1/RandomNumberWriter.java
@@ -1,14 +1,14 @@
 /** EXERCISE:
-   In this exercise you will write a Java application that writes 10,000 random numbers via a byte-based and a char-based
-   stream to a file.
+    In this exercise you will write a Java application that writes 10,000 random numbers via a byte-based and a char-based
+    stream to a file.
 
-   Write a class called RandomNumberWriter that implements RandomIO (package uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io).
-   There must be a constructor that takes one long as an argument. This long has to be used to seed the object's random
-   number generator (java.util.Random).
+    Write a class called RandomNumberWriter that implements RandomIO (package uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io).
+    There must be a constructor that takes one long as an argument. This long has to be used to seed the object's random
+    number generator (java.util.Random).
 
-   Implement the two methods writeRandomByte and writeRandomChars, which should write the 10,000 integers to the specified
-   file using a byte-based and a char-based stream, respectively. Each of these integers should be between 0 and 100,000
-   (including 0, but not 100,000).*/
+    Implement the two methods writeRandomByte and writeRandomChars, which should write the 10,000 integers to the specified
+    file using a byte-based and a char-based stream, respectively. Each of these integers should be between 0 and 100,000
+    (including 0, but not 100,000).*/
 
 import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io.RandomIO;
 import java.io.*;
